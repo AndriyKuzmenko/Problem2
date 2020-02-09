a7568a0103dc1bad857f2db6a39fb57ddf9333a9
Andriy <ak8022@bs.amalnet.k12.il>
2020-02-09 20:27:31 +0200

    Final push

--- app/src/main/java/com/example/problem2/MainActivity.java
+++ app/src/main/java/com/example/problem2/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.problem2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    Button btn;
+    int i;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn=(Button)findViewById(R.id.button);
+        i=0;
+    }
+
+    public void ButtonPressed(View view)
+    {
+        i++;
+        btn.setText("This is the click number: "+i);
     }
 }
